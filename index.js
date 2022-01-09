418658e4d2175ac940e3a5878e83d8818e9d2735
chrisk_rebot <chrisk_rebot@protonmail.com>
2022-01-09 12:33:40 +0800

    add

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const Wallet = require('./utils/wallet');
+const Transaction =  require('./utils/transfer');
+const TokenERC20 =  require('./utils/tokenERC20');
 
 // without default
 module.exports = Wallet;
+module.exports = Transaction;
+module.exports = TokenERC20;
